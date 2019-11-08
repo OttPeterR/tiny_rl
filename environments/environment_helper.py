6bf3dd46b64692d23ffe848a92f8cd379ea9bd8b
Peter Ott <ott.peter.r@gmail.com>
2019-11-07 22:28:48 -0500

    adding actions for environments

--- environments/environment_helper.py
+++ environments/environment_helper.py
@@ -18,8 +18,8 @@ def ShockwaveEnvironment(level):
 ###
 
 env_dict = {
-    "coin_collector":CoinCollectorEnvironment,
-    "shockwave":ShockwaveEnvironment,
+    "coin_collector":(CoinCollectorEnvironment, 4),
+    "shockwave":(ShockwaveEnvironment, 4),
 }
 ui_dict = {
     "coin_collector":CoinCollectorUI,
@@ -35,11 +35,11 @@ def getEnvironment(name):
     if name not in env_dict:
         raise(f"{name} not available. Valid choices: {env_dict.keys()}")
         return None
-    return env_dict[name]
+    return env_dict[name][0](), env_dict[name][1]
 
 def getEnvironment(name):
     if name not in ui_dict:
         raise(f"{name} not available. Valid choices: {ui_dict.keys()}")
         return None
-    return ui_dict[name]
+    return ui_dict[name]()
 
