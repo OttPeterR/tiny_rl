62309b90b746c31a0ece30ba57a7783738956ddb
Peter Ott <OttPeterR@users.noreply.github.com>
2019-11-12 12:37:01 -0600

    creating file for rainbow agent

---
+++ agents/rainbow.py
@@ -0,0 +1,25 @@
+import abc
+import six
+
+@six.add_metaclass(abc.ABCMeta)
+class Agent():
+
+    @abc.abstractmethod
+    def __init__(self, actions, sample_state):
+        pass
+
+    @abc.abstractmethod
+    def act(self, state):
+        pass
+
+    @abc.abstractmethod
+    def learn(self, state, action, reward, game_over):
+        pass
+
+    @abc.abstractmethod
+    def checkpoint(self, filename):
+        pass
+
+    @abc.abstractmethod
+    def load(self, checkpoint_file):
+        pass
