222e1d07fa771fc1f874d9822af8ada63eb7466a
Peter Ott <ott.peter.r@gmail.com>
2019-11-16 23:30:22 -0500

    tensorflow powered random

--- agents/deep_nn_agent.py
+++ agents/deep_nn_agent.py
@@ -1,19 +1,28 @@
 from agents import agent
 import numpy as np
-import tensorflow
+import tensorflow as tf
 
 def _construct_model(inputs,outputs):
     return None
 
 class DeepNNAgent(agent.Agent):
-    def __init__(self, actions, sample_state):
+    def __init__(self, actions, sample_state, *, seed=42, tf_sess=None):
         # super(DeepNNAgent).__init__(actions, sample_state)
         self.actions=actions
         self.model = _construct_model(sample_state, actions)
-
-
+        tf.compat.v1.set_random_seed(seed)
+        if tf_sess is None:
+            self.tf_sess = tf.compat.v1.Session()
+        else:
+            self.tf_sess = tf_sess
+    
+    def getTFSession(self):
+        return self.tf_sess
+    
     def act(self, state):
-        return np.random.choice(self.actions)
+        with self.tf_sess.as_default():
+            rand_act = (self.actions+1) * tf.compat.v1.random_uniform(())
+            return int(rand_act.eval())
 
     def learn(self, state, actions, reward, game_over):
         pass
